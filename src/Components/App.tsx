2cc160c7396cfbf3cb931ea0b5b84acc1a64ded3
Anthony Shellman <atshellman@gmail.com>
2022-09-11 11:38:51 -0600

    Add directory for components

---
+++ src/Components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Styles/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Test ... 1, 2, 3
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
