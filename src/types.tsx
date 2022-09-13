2e4863201ad33b0e768453d96b016fa5e0124f75
Anthony Shellman <atshellman@gmail.com>
2022-09-12 20:27:17 -0600

    Add types

--- src/types.tsx
+++ src/types.tsx
@@ -1,3 +1,18 @@
 export type AppStateStructure = {
         name: string
     }[]
+
+ export type FormInputs = {
+        name: string
+        race: string
+        classs: string
+        hp: number
+        ac: number
+        str: number
+        con: number
+        dex: number
+        wis: number
+        int: number
+        cha: number
+        about: string
+    }
