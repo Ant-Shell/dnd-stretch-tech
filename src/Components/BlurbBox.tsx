c71a65b61fe92f076308d41e55b84791d2c94b43
Nick Liggett <nick.liggett@gmail.com>
2022-09-15 18:32:25 -0600

    Add a Welcome Message to the BlurbBox

--- src/Components/BlurbBox.tsx
+++ src/Components/BlurbBox.tsx
@@ -17,7 +17,20 @@ const BlurbBox:FC<Props> = ({ currentClass }) => {
 
     return (
         <div className="blurb-box">
-            {(currentClass) && <h1>The {currentClass}</h1>}
+            {(currentClass) ? <h1>The {currentClass}</h1>: 
+            <div>
+                <h1>Welcome to Dungeons & Documents</h1>
+                <p>Where are your character creation problems come to an end! To the left you will see the Character Creation Form. 
+                    There you can fill out all the neccessary information and submit your character. 
+                    Upon submission, your hero will appear in the "Party Block" to the right.
+                    Don't feel like rolling dice or making those big decisions? Not to worry! 
+                    Simply click the "Randomize" button and we'll take care of that for you!
+                    If you are unhappy with your character so far, just click the "Clear" button and start over!
+                </p><br></br>
+                <p>Oh! Don't forget to check out the Monster Manual Page to get the scoop on possible friends or foes you may encounter during your adventures!
+                    After all... preparation is paramount.
+                </p>
+            </div>}
             <article className="the-blurb">{theBlurb}</article>
         </div>
     )
