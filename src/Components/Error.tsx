cf945b4c76a3e68bfb36d05e9c5e3e91757aaf2a
Andrew Miller <andrew.r.miller45@gmail.com>
2022-09-17 09:45:42 -0700

    create error page and css file.  import and route into App

---
+++ src/Components/Error.tsx
@@ -0,0 +1,13 @@
+import {FC} from "react"
+//create css file
+
+const Error:FC = () => {
+    return (
+        <div>
+            <h1>Oh no! You seem to have taken a wrong turn along your quest. Please return home!</h1>
+            <img src="src/Styles/roll20"></img>
+        </div>
+    )
+}
+
+export default Error
