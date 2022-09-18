9485a1a8c07e204fa6d878a318f52a23ed5a14ff
Maya Kappen <mayakappen@gmail.com>
2022-09-17 21:11:44 -0600

    Add background data for randomizer

--- src/randomizerData.tsx
+++ src/randomizerData.tsx
@@ -51,3 +51,18 @@ export const classes: string[] = [
     'Warlock',
     'Wizard'
 ]
+
+export const backstories: string[] = [
+`You fought in a battle. You survived the battle, but you suffer from terrible nightmares in which you relive the experience.`,
+`You were accused of Theft. You were caught and convicted. You spent time in jail, chained to an oar, or performing hard labor. You served a sentence of 4 years or succeeded in escaping after that much time.`,
+`You gained a bit of good fortune. You found a riding horse. You gained a bit of good fortune. You found a treasure map. You suffered a tragedy. A family member or a close friend died.`,
+`You fell in with a gang of reprobates and ne'er-do wells, and learned your specialty from them.`, `You went on an adventure. You nearly died.`, `You went on an adventure. You suffered a grievous injury. Although the wound healed, it still pains you from time to time.`,
+`While wandering around in a forbidden place, you encountered an otherworldly being that offered to enter into a pact with you.`,
+`You always had a way with animals, able to calm them with a soothing word and a touch.`,
+`Your family recently came by its title, and that elevation thrust them into a new and strange world.`,
+`You spent a lot of time in the wilderness as a youngster, and you came to love that way of life.`,
+`You suffered a tragedy. A lover disappeared without a trace. You have been looking for that person ever since.`,
+`You impressed a wizard who said you were squandering your talents and should seek out an education to take advantage of your gifts.`,
+`You wanted fame and fortune, so you joined a mercenary company, selling your sword to the highest bidder.`,
+`Wanderlust caused you to leave your family to see the world. You look after yourself.`
+]
